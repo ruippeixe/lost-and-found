50aa1ecf1e8c3062a35eef14bd0a767c701089e6
ruippeixe <66218687+ruippeixe@users.noreply.github.com>
2024-01-05 13:04:37 +0000

    Add page styling and favicon

--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -1,12 +1,18 @@
 import { Link } from "react-router-dom";
+import "./error.scss";
+import "../styles/global.scss";
 
 const Error = () => {
   return (
-    <>
-      <div>sorry, page not found</div>
+    <div className="error">
+      <div className="title">Sorry, the page was not found.</div>
 
-      <Link to="/">go back to homepage</Link>
-    </>
+      <Link to="/">
+        <button type="button" className="btn inverse">
+          go back to homepage
+        </button>
+      </Link>
+    </div>
   );
 };
 
