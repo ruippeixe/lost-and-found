b2676776ff0e4803272d80cd2b2e4c27f8bdb78f
ruippeixe <66218687+ruippeixe@users.noreply.github.com>
2024-02-14 14:32:25 +0000

    Change database datatype date format

--- server/db/database.sql
+++ server/db/database.sql
@@ -7,8 +7,8 @@ CREATE TABLE `items` (
   `what` varchar(255) NOT NULL,
   `who` varchar(255) DEFAULT NULL,
   `place` varchar(255) NOT NULL,
-  `date` date NOT NULL,
+  `date` varchar(10) NOT NULL,
   `time` time NOT NULL,
   `email` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-)
+);
