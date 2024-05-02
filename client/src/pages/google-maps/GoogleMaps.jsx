04d6f3c2e5e4ff98df85f70799c7cd8deeac51bf
ruippeixe <66218687+ruippeixe@users.noreply.github.com>
2024-05-02 17:07:40 +0100

    Add feature to retrieve location data from Google Maps API upon map click event

--- client/src/pages/google-maps/GoogleMaps.jsx
+++ client/src/pages/google-maps/GoogleMaps.jsx
@@ -5,10 +5,34 @@ import { MAPS_API_KEY } from "../../../config";
 function GoogleMaps() {
   const position = { lat: 40.2, lng: -10.4 };
 
+  const handleMapClick = (event) => {
+    const { latLng } = event.detail;
+    const lat = latLng.lat;
+    const lng = latLng.lng;
+
+    fetchData(lat, lng);
+  };
+
+  async function fetchData(lat, lng) {
+    try {
+      const response = await fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${MAPS_API_KEY}`
+      );
+      const data = await response.json();
+      console.log(data.results[0].address_components[1].long_name);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+
   return (
     <APIProvider apiKey={MAPS_API_KEY}>
       <div style={{ height: "100vh", width: "100%" }}>
-        <Map defaultZoom={8} defaultCenter={position}></Map>
+        <Map
+          defaultZoom={8}
+          defaultCenter={position}
+          onClick={handleMapClick}
+        ></Map>
       </div>
     </APIProvider>
   );
